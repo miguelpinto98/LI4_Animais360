116b4cd9ad3a51ed7e10eab66e2b1335c5653ee9
Miguel Pinto <mcpinto98@gmail.com>
2014-05-13 15:31:41 +0100

    Base visual studio para o trabalho
    - A trabalhar na parte do HTML de LOGIN e REGISTAR
    - Fazer Modelos
    - Povoar Base de Dados
    -

---
+++ Animais360/Animais360/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Animais360.Startup))]
+namespace Animais360
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
