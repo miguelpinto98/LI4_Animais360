973a92986b7eab4846590b36ca908fab9b9015ef
Miguel Pinto <mcpinto98@gmail.com>
2014-05-14 12:27:14 +0100

    Registo e Login de Utilizadores
    - Começar com Perfil de Jogador Para Editar Resto dos Dados
    - Definir completamente o Modelo do Utilizador
    - Verificar como aceder aos dados Estatisticos da Página Inicial
    - Criar View Para Perfil + Controlador?

--- Animais360/Animais360/Controllers/HomeController.cs
+++ Animais360/Animais360/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Animais360.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,12 @@ namespace Animais360.Controllers
 {
     public class HomeController : Controller
     {
+        private Animais360Context db = new Animais360Context();
         public ActionResult Index()
         {
-            ViewBag.numero = 10;
+            
+            ViewBag.numero = db.Users.Count();
+            ViewBag.last = "Não Definido";
             return View();
         }
     }
