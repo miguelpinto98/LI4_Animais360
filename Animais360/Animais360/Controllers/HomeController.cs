661c92ba073127f45398bc235a901b96a693c9f3
maleite <maleite.cbc@gmail.com>
2014-05-20 20:48:18 +0100

    estatisticas

--- Animais360/Animais360/Controllers/HomeController.cs
+++ Animais360/Animais360/Controllers/HomeController.cs
@@ -12,9 +12,16 @@ namespace Animais360.Controllers
         private Animais360Context db = new Animais360Context();
         public ActionResult Index()
         {
-            
+            List<User> utilizadores = db.Users.Where(u => u.Estado == 1).ToList();
             ViewBag.numero = db.Users.Count();
-            ViewBag.last = "Não Definido";
+            ViewBag.last = "";//db.Users.OrderByDescending(u => u.DataRegisto.ToShortDateString()).ThenByDescending(u => u.DataRegisto.ToShortTimeString()).ToList();
+            int voltas = 0;
+            foreach (User u in utilizadores)
+            {
+                voltas = voltas + u.NrVoltas;
+            }
+            ViewBag.nVoltas = voltas;
+            ViewBag.conquistador = "Não Definido";
             return View();
         }
     }
