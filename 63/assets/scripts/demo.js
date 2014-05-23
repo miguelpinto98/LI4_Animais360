c813099145f61d14cc784c9c61ac3daa30b97c09
Inês Medeiros <63msim@gmail.com>
2014-05-23 12:21:21 +0100

    caixas para todos os tipos de desafios ;)

--- 63/assets/scripts/demo.js
+++ 63/assets/scripts/demo.js
@@ -1,11 +1,8 @@
 window.onload = function() {
 
-	/**
-	*
-	*	Demo 1: Elements
-	*
-	*/
+
 	var dropZoneOne = document.querySelector('#drop-target-one');
+	var dropZoneTwo = document.querySelector('#drop-target-two');
 	var dragElements = document.querySelectorAll('#drag-elements li');
 	var elementDragged = null;
 
@@ -35,13 +32,32 @@ window.onload = function() {
 		return false;
 	});
 
+
+	dropZoneTwo.addEventListener('dragover', function(e) {
+		if (e.preventDefault) {
+			e.preventDefault();
+		}
+
+		e.dataTransfer.dropEffect = 'move';
+
+		return false;
+	});
+
 	// Event Listener for when the dragged element enters the drop zone.
 	dropZoneOne.addEventListener('dragenter', function(e) {
 		this.className = "over";
 	});
 
+	dropZoneTwo.addEventListener('dragenter', function(e) {
+		this.className = "over";
+	});
+
 	// Event Listener for when the dragged element leaves the drop zone.
 	dropZoneOne.addEventListener('dragleave', function(e) {
+		this.className = "";
+	});
+
+	dropZoneTwo.addEventListener('dragleave', function(e) {
 		this.className = "";
 	});
 
@@ -53,7 +69,15 @@ window.onload = function() {
 		this.className = "";
 		this.innerHTML =e.dataTransfer.getData('text');
 
-	
+		return false;
+	});
+
+	dropZoneTwo.addEventListener('drop', function(e) {
+		if (e.preventDefault) e.preventDefault(); 
+  	if (e.stopPropagation) e.stopPropagation(); 
+
+		this.className = "";
+		this.innerHTML =e.dataTransfer.getData('text');
 
 		return false;
 	});
