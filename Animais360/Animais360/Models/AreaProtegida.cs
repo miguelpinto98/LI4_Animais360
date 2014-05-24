320f4e43d302f67ff693e3b896e27d9cf37d4203
Miguel Pinto <mcpinto98@gmail.com>
2014-05-24 03:04:42 +0100

    Adiconar Areas Protegidas

--- Animais360/Animais360/Models/AreaProtegida.cs
+++ Animais360/Animais360/Models/AreaProtegida.cs
@@ -12,19 +12,35 @@ namespace Animais360.Models
     {
         public int AreaProtegidaID { get; set; }
 
+        [Required]
         [Display(Name = "Nome Área Protegida *")]
         public string AreaNome { get; set; }
-        
+
+        [Required]
         [Display(Name = "Longitude *")]
         public string Longitude { get; set; }
-        
+
+        [Required]
         [Display(Name = "Latitude *")]
         public string Latitude { get; set; }
 
+        [Required]
         [Display(Name = "Descrição *")]
         public string Descricao { get; set; }
         public int Permitida { get; set; }
+
         public Pais Pais { get; set; }
         public virtual ICollection<Questao> Questoes { get; set; }
+
+        [NotMapped]
+        [Display(Name = "País Pertencente *")]
+        public int IdPais { get; set; }
+        
+        [NotMapped]
+        [Display(Name = "Se País Não Existir **")]
+        public int IdContinente { get; set; }
+
+        [NotMapped]
+        public string NomePais { get; set; }
     }
 }
