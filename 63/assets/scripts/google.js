82a9f265a9154cc873a0540aa8a06d131c8a3446
Inês Medeiros <63msim@gmail.com>
2014-05-25 11:56:36 +0100

    percurso com linha

--- 63/assets/scripts/google.js
+++ 63/assets/scripts/google.js
@@ -1,15 +1,18 @@
-//pontos
-var p1= new google.maps.LatLng(44.5403, 20);
-var p2= new google.maps.LatLng(44.5403, 50);
+var p1 = new google.maps.LatLng(44.5403, 20);
+var p2 = new google.maps.LatLng(44.5403, 50);
+var p3 = new google.maps.LatLng(44.54,75);
+
+var mapOptions = {
+        center: p1, zoom: 2, 
+        mapTypeId: google.maps.MapTypeId.TERRAIN,
+        panControl: false, zoomControl: false, mapTypeControl: false,
+        scaleControl: false, streetViewControl: false,
+        //overviewMapControl: boolean
+    };
 
 
-var map_canvas = document.getElementById('map_canvas');
-var map_options = {center: p1, zoom: 2, panControl: false, zoomControl: false, mapTypeControl: false,
-                   scaleControl: false, streetViewControl: false,//overviewMapControl: boolean
-               };
+var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-//MAPA
-var map = new google.maps.Map(map_canvas, map_options);
 
 
 function HomeControl(controlDiv, map) {
@@ -57,7 +60,7 @@ function HomeControl(controlDiv, map) {
     caixa.appendChild(content);
     
 
-    for(i=0;i<3;i++){
+    for(i=0;i<5;i++){
 
           var ap = document.createElement('div');
             ap.style.width='180px';
@@ -75,42 +78,29 @@ function HomeControl(controlDiv, map) {
             apT.style.paddingLeft = '6px';
             apT.style.paddingRight = '4px';
             apT.style.paddingTop= '4px';
-            apT.innerHTML = '<b>Geres, Braga, Portugal</b>';
+            apT.innerHTML = '<b><del>Geres, Braga, Portugal</del></b>';
 
            
             content.appendChild(ap);
             ap.appendChild(apT);
+
+            google.maps.event.addDomListener(ap, 'click', function() {
+            map.setCenter(p2); map.setZoom(8);
+            });
     }
-
-
-
-
-
-  google.maps.event.addDomListener(ap, 'click', function() {
-    map.setCenter(p2); map.setZoom(8);
-  });
-
-   google.maps.event.addDomListener(ap, 'click', function() {
-    map.setCenter(p2); map.setZoom(8);
-  });
 }
 
 
 
-
 function initialize() {
-    
-    var marker, marker2;
-    var info, infoW;
+    var marker = new google.maps.Marker({position: p1, map: map, icon:'http://maps.google.com/mapfiles/ms/icons/blue.png', title: 'Clique para info'});
+    var marker2 = new google.maps.Marker({position: p2,map: map, title: 'Clique para zoom'});
 
-    marker = new google.maps.Marker({position: p1, map: map, icon:'http://maps.google.com/mapfiles/ms/icons/blue.png', title: 'Clique para info'});
-    marker2 = new google.maps.Marker({position: p2,map: map, title: 'Clique para zoom'});
-
-    info = '<div id="content">'+'<div id="siteNotice">'+'Braga, Portugal'+'</div>'+
+    var info = '<div id="content">'+'<div id="siteNotice">'+'Braga, Portugal'+'</div>'+
       '<h1 id="firstHeading" class="firstHeading">Gerês</h1>'+
       '<div id="bodyContent">'+'<p>Esta área é muito <b>importante</b> porque sim.</p>'+'</div>'+'</div>';
 
-    infoWindow = new google.maps.InfoWindow({content: info});
+    var infoWindow = new google.maps.InfoWindow({content: info});
 
     //Função que muda a "camara" para o ponto marcado, 3 segundos depois de mudar de sítio.
     /*google.maps.event.addListener(map, 'center_changed', function(){window.setTimeout( function(){map.panTo(marker.getPosition());}, 3000);});*/
@@ -130,23 +120,27 @@ function initialize() {
     var homeControl = new HomeControl(homeControlDiv, map);
     homeControlDiv.index = 1;
     map.controls[google.maps.ControlPosition.TOP_RIGHT].push(homeControlDiv);
+
+    var flightPlanCoordinates = [
+        p1,p2,p3
+    ];
+
+    var flightPath = new google.maps.Polyline({
+        path: flightPlanCoordinates,
+        geodesic: true,
+        strokeColor: '#FF0000',
+        strokeOpacity: 1.0,
+        strokeWeight: 2
+    });
+
+  flightPath.setMap(map);
 }
 
 
 google.maps.event.addDomListener(window, 'load', initialize);
-google.maps.event.addDomListener(window, 'load', function(){document.getElementById('dialog-anchor').click();});
+//google.maps.event.addDomListener(window, 'load', function(){document.getElementById('dialog-anchor').click();});
 
 
 if (typeof countitroundinstance === "undefined") {var countitroundinstance = [];}
         countitroundinstance.push({"unique":"countitround_LTSGPIDRAB","startdate":"0","enddate":"30","now":"0","color4":"#F39C12","backgroundcolor4":"#34495E","glowwidth4":"0","backgroundwidth4":"1","frontwidth4":"4","size4":"30"});
-      
-
-
-
-
-(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-    function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-    e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-    e.src='//www.google-analytics.com/analytics.js';
-    r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-    ga('create','UA-48454066-1');ga('send','pageview');
+    
